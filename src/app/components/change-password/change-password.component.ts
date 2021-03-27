acc5a83bdf7f91d057a7e779871d856c342ae935
Anton Ralovets <by.ralovets@gmail.com>
2021-03-27 20:40:29 +0300

    .

--- src/app/components/change-password/change-password.component.ts
+++ src/app/components/change-password/change-password.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../services/auth/auth.service';
 import {TokenStorageService} from '../../services/token-storage/token-storage.service';
 import {Router} from '@angular/router';
@@ -7,124 +7,127 @@ import {log} from 'util';
 import {NgModel} from '@angular/forms';
 
 @Component({
-  selector: 'app-change-password',
-  templateUrl: './change-password.component.html',
-  styleUrls: ['./change-password.component.css']
+    selector: 'app-change-password',
+    templateUrl: './change-password.component.html',
+    styleUrls: ['./change-password.component.css']
 })
 export class ChangePasswordComponent implements OnInit {
 
-  form: any = {};
-  isLoggedIn = false;
-  isLoginFailed = false;
-  errorMessage = '';
+    form: any = {};
+    isLoggedIn = false;
+    isLoginFailed = false;
+    errorMessage = '';
 
-  constructor(private authService: AuthService, private tokenStorage: TokenStorageService, private router: Router) { }
+    isOldPasswordCorrect = true;
+    isNewPasswordCorrect = true;
+    isConfirmationCorrect = true;
 
-  ngOnInit(): void {
-    if (!this.tokenStorage.getToken()) {
-      this.router.navigate(['/signin']);
-    }
-  }
-
-  onSubmit(): void {
-    log(this.form.oldPassword);
-    log(this.form.newPassword);
-    this.authService.updatePassword({ oldPassword: this.form.oldPassword, newPassword: this.form.newPassword }).subscribe(
-      data => {
-        log('signupData' + data);
-        this.tokenStorage.saveToken(data.token);
-        this.tokenStorage.saveUser(data);
-
-        this.router.navigate(['']);
-      }, err => {
-        this.errorMessage = err.error.message;
-        this.isLoginFailed = true;
-      }
-    );
-  }
-
-  isOldPasswordCorrect: boolean = true;
-  isNewPasswordCorrect: boolean = true;
-  isConfirmationCorrect: boolean = true;
-
-  onOldPasswordChange(oldPassword: NgModel) {
-    if (!oldPassword.value) {
-          this.disableSubmitButton();
-          document.getElementById('oldPassword')?.classList.remove('is-valid');
-          document.getElementById('oldPassword')?.classList.remove('is-invalid');
-          return;
+    constructor(private authService: AuthService, private tokenStorage: TokenStorageService, private router: Router) {
     }
 
-    log("OLD PASSWORD: " + oldPassword.value);
-
-    this.isOldPasswordCorrect = oldPassword.value.length > 5;
-
-    if (this.isOldPasswordCorrect) {
-      document.getElementById('oldPassword')?.classList.add('is-valid');
-      document.getElementById('oldPassword')?.classList.remove('is-invalid');
-    } else {
-      document.getElementById('oldPassword')?.classList.add('is-invalid');
-      document.getElementById('oldPassword')?.classList.remove('is-valid');
+    ngOnInit(): void {
+        if (!this.tokenStorage.getToken()) {
+            this.router.navigate(['/signin']);
+        }
     }
-    this.updateSubmitButtonStatus();
-  }
 
-  onNewPasswordChange(newPassword: NgModel, confirmPassword: NgModel) {
-      if (!newPassword.value) {
-        this.disableSubmitButton();
+    onSubmit(): void {
+        log(this.form.oldPassword);
+        log(this.form.newPassword);
+        this.authService.updatePassword({oldPassword: this.form.oldPassword, newPassword: this.form.newPassword}).subscribe(
+            data => {
+                log('signupData' + data);
+                this.tokenStorage.saveToken(data.token);
+                this.tokenStorage.saveUser(data);
 
-        if (confirmPassword.value) {
-          this.isConfirmationCorrect = false;
-          document.getElementById('confirmPassword')?.classList.add('is-invalid');
-          document.getElementById('confirmPassword')?.classList.remove('is-valid');
-        } else {
-          document.getElementById('confirmPassword')?.classList.remove('is-valid');
-          document.getElementById('confirmPassword')?.classList.remove('is-invalid');
+                this.router.navigate(['']);
+            }, err => {
+                this.errorMessage = err.error.message;
+                this.isLoginFailed = true;
+            }
+        );
+    }
+
+    onOldPasswordChange(oldPassword: NgModel): void {
+        if (!oldPassword.value) {
+            this.disableSubmitButton();
+            document.getElementById('oldPassword')?.classList.remove('is-valid');
+            document.getElementById('oldPassword')?.classList.remove('is-invalid');
+            return;
         }
 
-        document.getElementById('newPassword')?.classList.remove('is-valid');
-        document.getElementById('newPassword')?.classList.remove('is-invalid');
-        return;
-      }
+        log('OLD PASSWORD: ' + oldPassword.value);
 
-      this.isNewPasswordCorrect = newPassword.value.length > 5;
+        this.isOldPasswordCorrect = oldPassword.value.length > 5;
 
-      if (this.isNewPasswordCorrect) {
-        document.getElementById('newPassword')?.classList.add('is-valid');
-        document.getElementById('newPassword')?.classList.remove('is-invalid');
-      } else {
-        document.getElementById('newPassword')?.classList.add('is-invalid');
-        document.getElementById('newPassword')?.classList.remove('is-valid');
-      }
-
-      if (newPassword.value == confirmPassword.value) {
-        this.enableSubmitButton()
-        this.isConfirmationCorrect = true;
-        document.getElementById('confirmPassword')?.classList.add('is-valid');
-        document.getElementById('confirmPassword')?.classList.remove('is-invalid');
-      } else {
-        this.disableSubmitButton();
-        this.isConfirmationCorrect = false;
-        document.getElementById('confirmPassword')?.classList.remove('is-valid');
-        document.getElementById('confirmPassword')?.classList.add('is-invalid');
-      }
-
-      this.updateSubmitButtonStatus();
-  }
-
-  updateSubmitButtonStatus() {
-    if (this.isOldPasswordCorrect && this.isNewPasswordCorrect && this.isConfirmationCorrect && this.form.oldPassword && this.form.newPassword && this.form.confirmPassword) {
-      this.enableSubmitButton();
-    } else {
-      this.disableSubmitButton();
+        if (this.isOldPasswordCorrect) {
+            document.getElementById('oldPassword')?.classList.add('is-valid');
+            document.getElementById('oldPassword')?.classList.remove('is-invalid');
+        } else {
+            document.getElementById('oldPassword')?.classList.add('is-invalid');
+            document.getElementById('oldPassword')?.classList.remove('is-valid');
+        }
+        this.updateSubmitButtonStatus();
     }
-  }
 
-  disableSubmitButton() {
-    document.getElementById('submit')?.classList.add('disabled');
-  }
+    onNewPasswordChange(newPassword: NgModel, confirmPassword: NgModel): void {
+        if (!newPassword.value) {
+            this.disableSubmitButton();
 
-  enableSubmitButton() {
-    document.getElementById('submit')?.classList.remove('disabled');
-  }
+            if (confirmPassword.value) {
+                this.isConfirmationCorrect = false;
+                document.getElementById('confirmPassword')?.classList.add('is-invalid');
+                document.getElementById('confirmPassword')?.classList.remove('is-valid');
+            } else {
+                document.getElementById('confirmPassword')?.classList.remove('is-valid');
+                document.getElementById('confirmPassword')?.classList.remove('is-invalid');
+            }
+
+            document.getElementById('newPassword')?.classList.remove('is-valid');
+            document.getElementById('newPassword')?.classList.remove('is-invalid');
+            return;
+        }
+
+        this.isNewPasswordCorrect = newPassword.value.length > 5;
+
+        if (this.isNewPasswordCorrect) {
+            document.getElementById('newPassword')?.classList.add('is-valid');
+            document.getElementById('newPassword')?.classList.remove('is-invalid');
+        } else {
+            document.getElementById('newPassword')?.classList.add('is-invalid');
+            document.getElementById('newPassword')?.classList.remove('is-valid');
+        }
+
+        if (newPassword.value === confirmPassword.value) {
+            this.enableSubmitButton();
+            this.isConfirmationCorrect = true;
+            document.getElementById('confirmPassword')?.classList.add('is-valid');
+            document.getElementById('confirmPassword')?.classList.remove('is-invalid');
+        } else {
+            this.disableSubmitButton();
+            this.isConfirmationCorrect = false;
+            document.getElementById('confirmPassword')?.classList.remove('is-valid');
+            document.getElementById('confirmPassword')?.classList.add('is-invalid');
+        }
+
+        this.updateSubmitButtonStatus();
+    }
+
+    updateSubmitButtonStatus(): void {
+        if (this.isOldPasswordCorrect && this.isNewPasswordCorrect
+            && this.isConfirmationCorrect && this.form.oldPassword
+            && this.form.newPassword && this.form.confirmPassword) {
+            this.enableSubmitButton();
+        } else {
+            this.disableSubmitButton();
+        }
+    }
+
+    disableSubmitButton(): void {
+        document.getElementById('submit')?.classList.add('disabled');
+    }
+
+    enableSubmitButton(): void {
+        document.getElementById('submit')?.classList.remove('disabled');
+    }
 }
